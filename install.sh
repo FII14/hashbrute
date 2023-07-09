2dec95c0956cf5a71a17aa07c8eea4619aae54a1
FII14 <121723975+FII14@users.noreply.github.com>
2023-07-09 08:08:41 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# @ Script  : Instal hashbrute
+# @ Pembuat : Rofi
+
+g="\e[1;32m"
+r="\e[0m"
+c="\e[1;36m"
+
+clear
+
+echo -e "${c} ___ _   _ ____ _____  _    _     _     "
+echo -e "${c}|_ _| \ | / ___|_   _|/ \  | |   | |    "
+echo -e "${c} | ||  \| \___ \ | | / _ \ | |   | |    "
+echo -e "${c} | || |\  |___) || |/ ___ \| |___| |___ "
+echo -e "${c}|___|_| \_|____/ |_/_/   \_\_____|_____|"                                         
+echo -e "${r}"
+chmod +x src/hashbrute
+
+# Android (Termux)
+if [[ $(uname -o) == "Android" ]]; then
+    pkg update
+    pkg install python3
+    pip3 install -r requirements.txt
+    mv src/hashbrute /data/data/com.termux/files/usr/bin
+    echo -e "${g}[•] ${r}Installation completed."
+    echo -e "${g}[•] ${r}You can run it by executing the command '${g}hashbrute${r}'"
+    exit 0
+
+# Linux Ubuntu dan Debian beserta keturunannya
+elif [[ $(uname -o) == "GNU/Linux" ]]; then
+    sudo apt-get update 
+    sudo apt-get install python3-pip
+    pip3 install -r requirements.txt 
+    mv src/hashbrute /usr/bin
+    echo -e "${g}[•] ${r}Installation completed."
+    echo -e "${g}[•] ${r}You can run it by executing the command '${g}hashbrute${r}'"
+    exit 0
+fi
