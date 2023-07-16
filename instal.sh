18c2cd9b60da3be8d86b3eda8aad1ab770cce08e
FII14 <121723975+FII14@users.noreply.github.com>
2023-07-16 15:16:10 +0700

    Update instal.sh

--- instal.sh
+++ instal.sh
@@ -14,7 +14,32 @@ echo -e "${c} | ||  \| \___ \ | | / _ \ | |    "
 echo -e "${c} | || |\  |___) || |/ ___ \| |___ "
 echo -e "${c}|___|_| \_|____/ |_/_/   \_\_____|"                                         
 echo -e "${r}"
+
+# Periksa dan berikan izin eksekusi pada file hashbrute
 chmod +x src/hashbrute
+
+# Fungsi untuk menampilkan pesan selesai instalasi
+pesan(){
+    echo -e "${g}[•] ${r}Instalasi selesai."
+    echo -e "${g}[•] ${r}Anda dapat menjalankannya dengan menjalankan perintah '${g}hashbrute${r}'"
+    exit 0
+}
+
+# Fungsi untuk memeriksa dan mendapatkan rockyou.txt
+rockyou(){
+    if [[ -f "rockyou.txt" ]]; then
+        pesan
+    elif [[ -f "rockyou.txt.gz" ]]; then
+        gzip -d "rockyou.txt.gz"
+        pesan
+    elif [[ -f "rockyou.txt.gx" && -f "rockyou.txt" ]]; then
+        pesan
+    else
+        wget https://gitlab.com/kalilinux/packages/wordlists/-/raw/kali/master/rockyou.txt.gz
+        gzip -d "rockyou.txt.gz"
+        pesan
+    fi
+}
 
 # Android (Termux)
 if [[ $(uname -o) == "Android" ]]; then
@@ -24,27 +49,7 @@ if [[ $(uname -o) == "Android" ]]; then
     pip install -r persyaratan.txt
     mv src/hashbrute /data/data/com.termux/files/usr/bin
 
-    pesan(){
-        echo -e "${g}[•] ${r}Instalasi selesai."
-        echo -e "${g}[•] ${r}Anda dapat menjalankannya dengan menjalankan perintah '${g}hashbrute${r}'"
-        exit 0
-    }
-
-    rockyou(){
-        if [[ -f "rockyou.txt.gz" ]]; then
-            gzip -d "rockyou.txt.gz"
-            pesan
-        elif [[ -f "rockyou.txt.gx" && -f "rockyou.txt" ]]; then
-            pesan
-        elif [[ -f "rockyou.txt" ]]; then
-            pesan
-        else
-            wget https://gitlab.com/kalilinux/packages/wordlists/-/raw/kali/master/rockyou.txt.gz
-            gzip -d "rockyou.txt.gz"
-            pesan
-        fi
-    }
-    
+    # Periksa dan siapkan direktori wordlists
     direktori="/data/data/com.termux/files/usr/share/wordlists"
     if [[ -d "${direktori}" ]]; then
         cd "${direktori}"
@@ -63,28 +68,8 @@ elif [[ $(uname -o) == "GNU/Linux" ]]; then
     sudo apt-get install python3-pip
     pip3 install -r persyaratan.txt 
     mv src/hashbrute /usr/bin
-    
-    pesan(){
-        echo -e "${g}[•] ${r}Instalasi selesai."
-        echo -e "${g}[•] ${r}Anda dapat menjalankannya dengan menjalankan perintah '${g}hashbrute${r}'"
-        exit 0
-    }
 
-    rockyou(){
-        if [[ -f "rockyou.txt.gz" ]]; then
-            gzip -d "rockyou.txt.gz"
-            pesan
-        elif [[ -f "rockyou.txt.gx" && -f "rockyou.txt" ]]; then
-            pesan
-        elif [[ -f "rockyou.txt" ]]; then
-            pesan
-        else
-            wget https://gitlab.com/kalilinux/packages/wordlists/-/raw/kali/master/rockyou.txt.gz
-            gzip -d "rockyou.txt.gz"
-            pesan
-        fi
-    }
-    
+    # Periksa dan siapkan direktori wordlists
     direktori="/usr/share/wordlists"
     if [[ -d "${direktori}" ]]; then
         cd "${direktori}"
